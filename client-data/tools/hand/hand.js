0497c92541189f2050a774d68dd31c85cce2a616
Kareem Ahmad <kareemahmad@berkeley.edu>
2021-04-25 18:44:02 -0600

    Bounding-Box-based nearest object selection in move tool

--- client-data/tools/hand/hand.js
+++ client-data/tools/hand/hand.js
@@ -27,25 +27,73 @@
 (function hand() { //Code isolation
 	var selected = null;
 	var last_sent = 0;
+    var ALLOW_SEARCH = true; // search for selected element when none is selected.
+    var PATH_STEP_SIZE = 50;
 
+    // Helper functions
+    function distanceFromCursor(evt, stroke) {
+        var bbox = stroke.getBoundingClientRect(); // Relative to window
+        x = evt.clientX; // Relative to window
+        y = evt.clientY;
+
+        if ((x < bbox.x) || (y < bbox.y) || (y > bbox.y + bbox.height) || (x > bbox.x + bbox.width))
+            return Number.MAX_VALUE;
+
+        if (stroke.tagName == 'text' || stroke.tagName == 'svg') {
+            console.log("  is text/svg");
+            return 0;
+        }
+
+        var d, dist=Number.MAX_VALUE;
+        for (d=0; d < stroke.getTotalLength(); d+=PATH_STEP_SIZE) {
+            var pt = stroke.getPointAtLength(d);
+            dist = Math.min(dist, Math.abs(pt.x - x) + Math.abs(pt.y - y));
+        }
+        return dist;
+    }
+
+    // Main Code
 
 	function startMovingElement(x, y, evt) {
 		//Prevent the press from being interpreted by the browser
-        console.log(evt);
+        console.log(evt, x, y);
 		evt.preventDefault();
-		if (!evt.target || !Tools.drawingArea.contains(evt.target)) return;
+        var target = null;
+        if (!evt.target || !Tools.drawingArea.contains(evt.target)) { 
+            if (!ALLOW_SEARCH || (evt.target != canvas)) return; 
+
+            var draw_area = document.getElementById("drawingArea")
+            var i, min_dist = Number.MAX_VALUE;
+            var min_child = null;
+
+            for (i=0; i < draw_area.childElementCount; i++) {
+                var child = draw_area.children[i];
+                var dist  = distanceFromCursor(evt, child);
+                if (dist < min_dist) {
+                    min_child = child;
+                    min_dist  = dist;
+                }
+            }
+            if (!min_child) return; 
+            target = min_child;
+        } else {
+            // directly selected target. 
+            console.log("traversing parentage", evt.target);
+            target = evt.target;
+            // Check for parent that is MathElement
+		    var a = target;
+		    var els = [];
+		    while (a) {
+		    	els.unshift(a);
+		    	a = a.parentElement;
+		    }
+		    var parentMathematics = els.find(el => el.getAttribute("class") === "MathElement");
+		    if ((parentMathematics) && parentMathematics.tagName === "svg") {
+		    	target = parentMathematics;
+		    }
+            console.log("final parent", target);
+        }
 		// search for a parent that is a MathElement. If one is found then act on that instead.
-		var target = evt.target;
-		var a = target;
-		var els = [];
-		while (a) {
-			els.unshift(a);
-			a = a.parentElement;
-		}
-		var parentMathematics = els.find(el => el.getAttribute("class") === "MathElement");
-		if ((parentMathematics) && parentMathematics.tagName === "svg") {
-			target = parentMathematics;
-		}
 		var tmatrix = get_translate_matrix(target);
 		selected = { x: x - tmatrix.e, y: y - tmatrix.f, elem: target };
 	}
@@ -84,7 +132,6 @@
 	}
 
 	function draw(data) {
-        console.log("draw");
 		switch (data.type) {
 			case "update":
 				var elem = Tools.svg.getElementById(data.id);
